56e2277c5994c3a90316f47794c9bc63199b96e4
sharjeelyunus <sharjeel924@gmail.com>
2021-10-11 11:39:29 +0500

    📦 NEW: Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="app__header">
+        <h1>Open Source Contributors</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
