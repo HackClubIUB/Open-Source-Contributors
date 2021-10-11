86bb56107126295591984d6f86545d7e7cabc5fd
sharjeelyunus <sharjeel924@gmail.com>
2021-10-11 12:22:20 +0500

    🚀 RELEASE: Open Source Contributors Directory

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Users from './components/Users';
+import FetchUser from './components/FetchUser';
 
 function App() {
   return (
@@ -6,6 +8,25 @@ function App() {
       <header className="app__header">
         <h1>Open Source Contributors</h1>
       </header>
+      <div className="app__desc">
+        <h2>Let's Contribute To open-source</h2>
+        <p>
+          Here is the Open Source Contributors Directory. Open your first Pull Request. <br />
+          Read the Contribution Guidelines from&nbsp;
+          <a
+            href="https://github.com/HackClubIUB/Open-Source-Contributors/blob/master/README.md"
+            target="_blank"
+            rel="noreferrer"
+          >
+            here!
+          </a>
+        </p>
+      </div>
+
+      <div className="users">
+        <FetchUser />
+        <Users />
+      </div>
     </div>
   );
 }
